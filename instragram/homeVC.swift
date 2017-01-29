181ce1cf14bacbf122d912d03d454ae02cbb9470
bingkunyang <bingkunyang@bingkunyangdeMacBook-Pro.local>
2017-01-29 10:23:09 -0800

    commit for receiving user information from the server for Home Page

--- instragram/homeVC.swift
+++ instragram/homeVC.swift
@@ -7,51 +7,58 @@
 //
 
 import UIKit
+import Parse
 
-private let reuseIdentifier = "Cell"
 
 class homeVC: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
-        // Do any additional setup after loading the view.
+        
+        // background color
+        collectionView?.backgroundColor = .white
+        
+        // title at the top
+        self.navigationItem.title = PFUser.current()?.username?.uppercased()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return 0
     }
+    
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        
+        //define header
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "Header", for: indexPath) as! headerView
+        
+        
+        // get users data with connections to columns of PFUser class
+        header.fullNameLbl.text = (PFUser.current()!.object(forKey: "fullname") as? String)?.uppercased()
+        header.webTxt.text = PFUser.current()?.object(forKey: "web") as? String
+        header.webTxt.sizeToFit()
+        header.bioLbl.text = PFUser.current()?.object(forKey: "bio") as? String
+        header.bioLbl.sizeToFit()
+        header.button.setTitle("edit profile", for: UIControlState.normal)
+        
+        let avaQuery = PFUser.current()?.object(forKey: "ava") as! PFFile
+        avaQuery.getDataInBackground { (data: Data?, error: Error?) in
+            header.avaImg.image = UIImage(data: data!)
+            
+        }
+        
+        return header
+        
+    }
 
+    
+    /*
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
     
@@ -59,6 +66,7 @@ class homeVC: UICollectionViewController {
     
         return cell
     }
+    */
 
     // MARK: UICollectionViewDelegate
 
