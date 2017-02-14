28573665ec73894df7045df4cac3e706c2623400
bingkunyang <bingkunyang@bingkunyangdeMacBook-Pro.local>
2017-02-14 11:36:28 -0800

    create followers and following user page

--- instragram/followersCell.swift
+++ instragram/followersCell.swift
@@ -7,21 +7,69 @@
 //
 
 import UIKit
+import Parse
 
 class followersCell: UITableViewCell {
     @IBOutlet weak var avaImg: UIImageView!
     @IBOutlet weak var usernameLbl: UILabel!
     @IBOutlet weak var followBtn: UIButton!
 
+    // default func
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        // round ava
+        avaImg.layer.cornerRadius = avaImg.frame.size.width / 2;
+        avaImg.clipsToBounds = true
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    // clicked follow/ unfollow
+    @IBAction func followBtn_click(_ sender: Any) {
+        
+        let title = followBtn.title(for: UIControlState())
+        
+        // to follow
+        if title == "FOLLOW" {
+            let object = PFObject(className: "follow")
+            object["follower"] = PFUser.current()?.username
+            object["following"] = usernameLbl.text
+            object.saveInBackground(block: { (success, error) -> Void in
+                if success {
+                    self.followBtn.setTitle("FOLLOWING", for: UIControlState())
+                    self.followBtn.backgroundColor = .green
+                } else {
+                    print(error?.localizedDescription)
+                }
+            })
+        }
+            
+        // unfollow
+        else {
+            let query = PFQuery(className: "follow")
+            query.whereKey("follower", equalTo: PFUser.current()!.username!)
+            query.whereKey("following", equalTo: usernameLbl.text!)
+            query.findObjectsInBackground(block: { (objects, error) -> Void in
+                if error == nil {
+                    
+                    for object in objects! {
+                        object.deleteInBackground(block: { (success, error) -> Void in
+                            if success {
+                                self.followBtn.setTitle("FOLLOW", for: UIControlState())
+                                self.followBtn.backgroundColor = .lightGray
+                            } else {
+                                print(error?.localizedDescription)
+                            }
+                        })
+                    }
+                    
+                } else {
+                    print(error?.localizedDescription)
+                }
+            })
+        }
     }
 
 }
+
+
--- instragram/followersVC.swift
+++ instragram/followersVC.swift
@@ -7,41 +7,194 @@
 //
 
 import UIKit
+import Parse
 
 var user = String()
 var category = String()
 
 
 class followersVC: UITableViewController {
+    
+    // arrays to hold data received from servers
+    var usernameArray = [String]()
+    var avaArray = [PFFile]()
+    
+    // array showing who do we follow or who followings us
+    var followArray = [String]()
 
+    
+    // default func
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // title at the top
         self.navigationItem.title = category.uppercased()
         
+        // load followers if tapped on followers label
+        if category == "followers" {
+            loadFollowers()
+        }
+        
+        // load followings if tapped on followings label
+        if category == "followings" {
+            loadFollowings()
+        }
+        
+    }
+    
+    
+    
+    // loading followers
+    func loadFollowers() {
+        
+        // STEP 1. Find in FOLLOW class people following User
+        // find followers of user
+        let followQuery = PFQuery(className: "follow")
+        followQuery.whereKey("following", equalTo: user)
+        followQuery.findObjectsInBackground (block: { (objects, error) -> Void in
+            if error == nil {
+                
+                // clean up
+                self.followArray.removeAll(keepingCapacity: false)
+                
+                // STEP 2. Hold received data
+                // find related objects depending on query settings
+                for object in objects! {
+                    self.followArray.append(object.value(forKey: "follower") as! String)
+                }
+                
+                // STEP 3. Find in USER class data of users following "User"
+                // find users following user
+                let query = PFUser.query()
+                query?.whereKey("username", containedIn: self.followArray)
+                query?.addDescendingOrder("createdAt")
+                query?.findObjectsInBackground(block: { (objects, error) -> Void in
+                    if error == nil {
+                        
+                        // clean up
+                        self.usernameArray.removeAll(keepingCapacity: false)
+                        self.avaArray.removeAll(keepingCapacity: false)
+                        
+                        // find related objects in User class of Parse
+                        for object in objects! {
+                            self.usernameArray.append(object.object(forKey: "username") as! String)
+                            self.avaArray.append(object.object(forKey: "ava") as! PFFile)
+                            self.tableView.reloadData()
+                        }
+                    } else {
+                        print(error!.localizedDescription)
+                    }
+                })
+                
+            } else {
+                print(error!.localizedDescription)
+            }
+        })
+        
+    }
+    
+    
+    
+    // loading followings
+    func loadFollowings() {
+        
+        // STEP 1. Find people followed by User
+        let followQuery = PFQuery(className: "follow")
+        followQuery.whereKey("follower", equalTo: user)
+        followQuery.findObjectsInBackground (block: { (objects, error) -> Void in
+            if error == nil {
+                
+                // clean up
+                self.followArray.removeAll(keepingCapacity: false)
+                
+                // STEP 2. Hold received data in followArray
+                // find related objects in "follow" class of Parse
+                for object in objects! {
+                    self.followArray.append(object.value(forKey: "following") as! String)
+                }
+                
+                // STEP 3. Basing on followArray information (inside users) show infromation from User class of Parse
+                // find users followeb by user
+                let query = PFQuery(className: "_User")
+                query.whereKey("username", containedIn: self.followArray)
+                query.addDescendingOrder("createdAt")
+                query.findObjectsInBackground(block: { (objects, error) -> Void in
+                    if error == nil {
+                        
+                        // clean up
+                        self.usernameArray.removeAll(keepingCapacity: false)
+                        self.avaArray.removeAll(keepingCapacity: false)
+                        
+                        // find related objects in "User" class of Parse
+                        for object in objects! {
+                            self.usernameArray.append(object.object(forKey: "username") as! String)
+                            self.avaArray.append(object.object(forKey: "ava") as! PFFile)
+                            self.tableView.reloadData()
+                        }
+                    } else {
+                        print(error!.localizedDescription)
+                    }
+                })
+                
+            } else {
+                print(error!.localizedDescription)
+            }
+        })
+        
     }
 
     
-    // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
+    // cell numb
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return usernameArray.count
     }
 
-    /*
+    
+    
+    // cell config
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        // define cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! followersCell
+        
+        // STEP 1. Connect data from serv to objects
+        cell.usernameLbl.text = usernameArray[indexPath.row]
+        avaArray[indexPath.row].getDataInBackground { (data, error) -> Void in
+            if error == nil {
+                cell.avaImg.image = UIImage(data: data!)
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
+        
+        
+        // STEP 2. Show do user following or do not
+        let query = PFQuery(className: "follow")
+        query.whereKey("follower", equalTo: PFUser.current()!.username!)
+        query.whereKey("following", equalTo: cell.usernameLbl.text!)
+        query.countObjectsInBackground (block: { (count, error) -> Void in
+            if error == nil {
+                if count == 0 {
+                    cell.followBtn.setTitle("FOLLOW", for: UIControlState())
+                    cell.followBtn.backgroundColor = .lightGray
+                } else {
+                    cell.followBtn.setTitle("FOLLOWING", for: UIControlState())
+                    cell.followBtn.backgroundColor = UIColor.green
+                }
+            }
+        })
+        
+        
+        // STEP 3. Hide follow button for current user
+        if cell.usernameLbl.text == PFUser.current()?.username {
+            cell.followBtn.isHidden = true
+        }
+        
         return cell
     }
-    */
+    
+    
 
     /*
     // Override to support conditional editing of the table view.
